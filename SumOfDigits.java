391a086c998ccdc1bcc538bf1fce92985cda65f4
VIGNESH G <98693579+VIGNESH2681@users.noreply.github.com>
2024-04-14 00:19:43 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,19 @@
+package com.vignesh.DSA;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    static int sumDigits(int n){
+        if(n == 0){
+            return 0;
+        }
+        return sumDigits(n/10) + n%10;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("The sum of digits is: " + sumDigits(n));
+    }
+}
