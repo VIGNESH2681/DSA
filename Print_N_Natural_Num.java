e266a9cf35ab35c62662e6cd5428f9fce62ece39
VIGNESH G <98693579+VIGNESH2681@users.noreply.github.com>
2024-04-13 20:03:47 +0530

    Add files via upload

---
+++ Print_N_Natural_Num.java
@@ -0,0 +1,15 @@
+package com.vignesh.DSA;
+
+public class Print_N_Natural_Num {
+
+    static void printN(int n){
+        if(n==0){
+            return;
+        }
+        printN(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        printN(5);
+    }
+}
