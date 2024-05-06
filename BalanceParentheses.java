4cba6f7d3f89ce246d676935cfed92cf2b13acef
VIGNESH G <98693579+VIGNESH2681@users.noreply.github.com>
2024-05-06 15:24:04 +0530

    Add files via upload

---
+++ BalanceParentheses.java
@@ -0,0 +1,26 @@
+package com.vignesh.DSA;
+
+import java.util.Scanner;
+
+public class BalanceParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] ar = new char[n*2];
+        balpara(ar,n,0,0,0);
+    }
+
+    static void balpara(char[] ar, int n, int i, int o, int c) {
+        if(i==ar.length){
+            System.out.println(ar);
+        }
+        if(o<n){
+            ar[i] = '(';
+            balpara(ar,n,i+1,o+1,c);
+        }
+        if(c<o){
+            ar[i] = ')';
+            balpara(ar,n,i+1,o,c+1);
+        }
+    }
+}
